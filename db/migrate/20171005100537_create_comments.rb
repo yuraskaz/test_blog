25cd245f0a121bd76659054b08bf0e5f7a9028fd
yuraskaz <kazachkoff.yury@gmail.com>
2017-10-06 11:19:01 +0300

    initial model post,user,comments

---
+++ db/migrate/20171005100537_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[5.1]
+  def change
+    create_table :comments do |t|
+      t.references :post, foreign_key: true
+      t.references :user, foreign_key: true
+      t.text :body
+      t.datetime :published_at
+
+      t.timestamps
+    end
+  end
+end
